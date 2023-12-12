b1c89556a5d70df6d70f602ca47c52ef173c5fc9
LearnerAFK <115334540+LearnerAFK@users.noreply.github.com>
2023-12-12 19:05:41 +0530

    Barbie v0.1

--- Barbie/core/mongo.py
+++ Barbie/core/mongo.py
@@ -5,5 +5,5 @@ from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 
 _mongo_async_ = _mongo_client_(config.MONGO_DB)
 _mongo_sync_ = MongoClient(config.MONGO_DB)
-mongodb = _mongo_async_.Altron
-pymongodb = _mongo_sync_.Altron
+mongodb = _mongo_async_.Barbie
+pymongodb = _mongo_sync_.Barbie
